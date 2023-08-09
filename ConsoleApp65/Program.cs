e582e5bd9829f0cc58cfa88dddf6a12af6aebe79
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 17:42:11 +0330

    Add files via upload

---
+++ ConsoleApp65/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+            string[] friends = new string[5];
+
+            friends[0] = "jim";
+            friends[1] = "kelly";
+
+            Console.WriteLine(luckyNumbers[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
